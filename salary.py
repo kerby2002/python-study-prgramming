c9ef612f1742f0b3c42824001df642cd54457c7a
koerby2002 <candice910203@gmail.com>
2023-01-14 17:06:16 +0800

    new

---
+++ salary.py
@@ -0,0 +1,13 @@
+import pandas as pd
+data = pd.read_excel()
+
+salary = int(input("請輸入薪資總額："))
+bonus = int(input("請輸入工作獎金："))
+t = int(input("請輸入加班時數："))
+d = int(input("請輸入病假天數："))
+a = int(input("請輸入事假天數："))
+
+
+totalsalary = salary + bonus + salary/240*t - d*salary/240*0.5 - a*salary/240
+
+print("本月實領金額："+str(totalsalary))
